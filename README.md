d5a2dcf7e14dc733dfc689d4f21058f1b0f83e14
Samuel Price <samjprice@yahoo.co.uk>
2021-08-29 17:42:34 +0100

    whv_manual_check now working with error message and contained within functions

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Tool for alerting me to the change in status for Japan accepting Working Holiday Visas 
 
 I now intend to make 2 variations of the tool
-WHV Manual Check - for launching from my desktop with notifications for all eventualities
-WHV Automated - for background checking without notifications if nothing has changed
+whv_manual_check - for launching from my desktop with notifications for all eventualities
+whv_automated - for background checking without notifications if nothing has changed
 
-I use Manjaro so I formatted it in cron using the following as per https://wiki.archlinux.org/title/Desktop_notifications
+Note: I use Manjaro so I formatted it in cron as per https://wiki.archlinux.org/title/Desktop_notifications
---
+++ whv_manual_check.py
@@ -0,0 +1,52 @@
+# This is my manual checker which always sends notifications
+
+import bs4
+import requests
+
+from notifypy import Notify
+
+
+APP_NAME = "WHV Notifier"
+site_url = "https://www.uk.emb-japan.go.jp/itpr_en/index_000072.html"
+
+
+def scrape_site(site_url: str) -> [str, str]:
+    res = requests.get(site_url)
+    try:
+        res.raise_for_status()
+
+        soup = bs4.BeautifulSoup(res.text, "html.parser")
+
+        warning = soup.select_one("#main > div:nth-child(4) > div > div > div > p > font > strong").text.strip()
+        last_updated = soup.select_one("#main > div:nth-child(2)").text.strip()
+    except:
+        pass
+
+    if warning and last_updated:
+        return warning, last_updated
+    else:
+        return
+
+
+def main():
+    target_date = "2021/7/1"
+    restriction = "Due to restrictions relating to the coronavirus pandemic, we are not able to take applications for the Working Holiday Visa."
+
+    try:
+        warning, last_updated = scrape_site(site_url)
+
+        if warning == restriction:
+            if last_updated == target_date:
+                message = f"No Update"
+            else:
+                message = f"Date Updated - Working Holiday Visas still not available \n{site_url}"
+        else:
+            message = f"See website for update \n{site_url}"
+    except:
+        message = f"Error - see website \n{site_url}"
+
+    Notify("Japan Working Holiday Visa", message, APP_NAME).send()
+
+
+if __name__ == "__main__":
+    main()
